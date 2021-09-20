cc97c439e4464b8172fd9575bc08a3ac9e0ce0ed
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-09-20 14:01:25 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -144,7 +144,7 @@ const DeleteItemHandler = {
         const {requestEnvelope, responseBuilder} = handlerInput;
         const {intent} = requestEnvelope.request;
 
-        const catalog = Alexa.getSlotValue(requestEnvelope, 'catalog');
+        const catalog = Alexa.getSlotValue(requestEnvelope, 'catalogues');
         const item = Alexa.getSlotValue(requestEnvelope, 'item');
         
         let speechText = ""
