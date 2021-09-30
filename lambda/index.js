82bec605afdf792705607f34d993e1d867a12945
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2021-09-30 17:20:32 +0530

    user id added

--- lambda/index.js
+++ lambda/index.js
@@ -510,6 +510,7 @@ const ViewCataloguesIntentHandler = {
         let decoded = jwt(accessToken)
         let userID = decoded.sub
         
+        // userID = '14082a4d-35d1-4450-97c3-393730cffa29'
         await getRemoteData(`https://v86cz5q48g.execute-api.us-east-1.amazonaws.com/dev/catalogue-by-user-id/${userID}`)
             .then((response) => {
                 const data = JSON.parse(response);
