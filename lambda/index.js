fec55f1e9a3665d62e73ed7dd3e0f94bdad0a97c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-09-30 17:06:36 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -7,7 +7,7 @@ const demo_data = require('./documents/demoData.json');
 const jwt = require("jwt-decode")
 //const main = require('./main.json');
 const {getRemoteData} = require('./api/api-get-data.js');
-const {deleteRequest} = require('./api/api-delete-data.js');
+// const {deleteRequest} = require('./api/api-delete-data.js');
 
 const postRequest = async (userID, catalog) => {
     try {
