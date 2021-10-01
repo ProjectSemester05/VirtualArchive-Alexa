78eb6f1736d7973ecfc7d20c60d8fa402827d80d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-10-01 01:29:07 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -64,7 +64,7 @@ const LaunchRequestHandler = {
         // }
         
         var date = today_date+'-'+month+'-'+today.getFullYear();
-        date = '01-10-2021'
+        // date = '01-10-2021'
         // userID = '14082a4d-35d1-4450-97c3-393730cffa29'
         await getRemoteData(`https://v86cz5q48g.execute-api.us-east-1.amazonaws.com/dev/reminder-by-user/${userID}`)
             .then((response) => {
